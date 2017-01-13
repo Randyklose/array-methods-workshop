b8131d2579c3968b85d5dc1c10f74953f69078e6
Randy Klose <randyklose@gmail.com>
2017-01-13 01:15:29 +0000

    Functional programming completed

--- answers.js
+++ answers.js
@@ -126,10 +126,9 @@ function highLow(arr) {
 console.log(highLow([10, 20, -10, -20]));
 
 /*## Wheel of Fortune
-Write a function called `countChars` that takes a string, and returns an object where the keys are letters, and the value is the number of times that letter appears.
-
+Write a function called `countChars` that takes a string, and returns an object where the keys are letters,
+and the value is the number of times that letter appears.
 For example, with input "hello world", the output should be:
-
 ```json
 {
   "h": 1,
@@ -141,11 +140,34 @@ For example, with input "hello world", the output should be:
   "d": 1
 }
 ```
+**NOTE**: Unlike arrays, objects don't have any ordering on them.
+When you print your object on the console, your keys may be displayed in a different order,
+and it does not matter.*/
 
-**NOTE**: Unlike arrays, objects don't have any ordering on them. When you print your object on the console, your keys may be displayed in a different order, and it does not matter.
+function countChars(str) {
+    return str.split("").reduce(function(acc, currentLetter) {
+        if(!acc[currentLetter]) {
+            acc[currentLetter] = 1;
+        }
+        else {
+            acc[currentLetter]++
+        }
+        return acc;
+    }, {})
+}
 
-## Functional Programming
-Let's take a break from calling array methods and think about a certain situation that occurs with the `filter` method. `Array.prototype.filter` will filter out elements where the callback function returns a falsy value. What if we wanted to do the opposite, and filter out elements which return a truthy value? Here's an example:
+// function countChar1Line(str) {
+//     return str.split("").reduce((a,b) => {!a.b ? a.b=1 : a.b++; return a}, {})
+// }
+
+console.log(countChars("hello world"));
+
+
+/*## Functional Programming
+Let's take a break from calling array methods and think about a certain situation that occurs
+with the `filter` method. `Array.prototype.filter` will filter out elements where the callback function
+returns a falsy value. What if we wanted to do the opposite, and filter out elements which 
+return a truthy value? Here's an example:
 
 ```javascript
 function isEven(num) {
@@ -156,7 +178,8 @@ var numbers = [4,8,15,16,23,42];
 var evenNumbers = numbers.filter(isEven);
 ```
 
-If we want to filter for odd numbers, we have to create a new function that returns true if a number is odd. Here's one way:
+If we want to filter for odd numbers, we have to create a new function that returns true if a number is odd. 
+Here's one way:
 
 ```javascript
 function isOdd(num) {
@@ -184,17 +207,27 @@ function isNotEmpty(someList) {
 }
 ```
 
-There's a common pattern repeating itself here: whenever we want to create a function that negates the result of another function, we're simply calling the original function with the same parameters, and prefixing the call with a `!`. This will invert the boolean result from the original function, giving us exactly what we need.
+There's a common pattern repeating itself here: whenever we want to create a function that negates the result
+of another function, we're simply calling the original function with the same parameters, 
+and prefixing the call with a `!`. 
+This will invert the boolean result from the original function, giving us exactly what we need.
 
-However, we can do better than writing out a new function manually every time we want to create a negated version of a function. Let's use higher-order functions to do just that.
+However, we can do better than writing out a new function manually every time we want to create a
+negated version of a function. Let's use higher-order functions to do just that.
 
-In this exercise, **your goal is to create a `negate` function**. `negate` takes one parameter: another function. `negate` should **return a function** that, when called, will return the opposite of what the input function would return on the same input.
+In this exercise, **your goal is to create a `negate` function**. `negate` takes one parameter: another function. 
+`negate` should **return a function** that, when called, will return the opposite of what the
+input function would return on the same input.
 
-To make things simpler, **you can assume that the original function only takes one parameter**. Here's how I should be able to use your code:
+To make things simpler, **you can assume that the original function only takes one parameter**. 
+Here's how I should be able to use your code:
 
-```javascript
+```javascript */
 function negate(fn) {
-  // your solution goes here
+        return function(x) {
+            return !fn(x);
+        };
+  // your solution goes here  x => !fn(x)
 }
 
 // Original functions
@@ -207,9 +240,166 @@ function isEmpty(someList) {
 
 // New functions
 var isOdd = negate(isEven);
-var isNotEmpty = negate(empty);
+var isNotEmpty = negate(isEmpty);
 
-[1,2,3].filter(isEven) // [2]
-[1,2,3].filter(isOdd) // [1,3]
+console.log([1,2,3].filter(isEven)); // [2]
+console.log([1,2,3].filter(isOdd)); // [1,3]
+
+
+
+/*### Challenge
+As a challenge, provide a more complete version of the `negate` function that will work with 
+**any number of parameters**. For example, it should work with the following:
+Here, since the original function takes two parameters, your `negate` should call it with two parameters.
+But for this challenge, you should make `negate` work for an arbitrary number of parameters.
+Hint: you can use the `arguments` keyword :)
+```javascript*/
+function negate2(fn) {
+        return function() {
+            return !fn.apply(this, arguments)
+        };
+   //your solution goes here
+}
+
+
+function firstDividesSecond(first, second) {
+  return second % first === 0;
+}
+
+var firstDoesNotDivideSecond = negate2(firstDividesSecond);
+
+console.log(firstDividesSecond(10,5));
+
+console.log(firstDoesNotDivideSecond(10, 5));
+
+
+/*## Find by ID
+Starting on week 3 of the bootcamp, we will be learning about databases. 
+Databases are useful for saving data for long periods of time.
+
+Contrary to a JavaScript program, where all the data disappears when the program terminates,
+databases keep their data "forever".
+
+In this exercise, we're going to imagine that we are storing people information in a database,
+and that we queried our database to retrieve a list of people.
+
+Our database returns to us an array of people objects, and each of them has a **unique ID** that the database 
+uses to refer to it.
+
+Here's what our person data could look like:
+
+```json
+[
+  {
+    "id": "KeXoYg92is",
+    "firstName": "John",
+    "lastName": "Smith",
+    "email": "john@smith.com"
+  },
+  {
+    "id": "NkALmSWtUp",
+    "firstName": "Donald",
+    "lastName": "Duck",
+    "email": "don@disney.com"
+  },
+  {
+    "id": "m7LPbJYSUg",
+    "firstName": "John",
+    "lastName": "Vader",
+    "email": "vader@darkside.com"
+  }
+]
+```
+
+For this exercise, we want to use `Array.prototype.reduce` to transform our array of people into an object, 
+keyed with the unique ID.
+
+The end result should look like this:
+
+```json
+{
+  "KeXoYg92is": {
+    "id": "KeXoYg92is",
+    "firstName": "John",
+    "lastName": "Smith",
+    "email": "john@smith.com"
+  },
+  "NkALmSWtUp": {
+    "id": "NkALmSWtUp",
+    "firstName": "Donald",
+    "lastName": "Duck",
+    "email": "don@disney.com"
+  },
+  "m7LPbJYSUg": {
+    "id": "m7LPbJYSUg",
+    "firstName": "John",
+    "lastName": "Vader",
+    "email": "vader@darkside.com"
+  }
+}
+```
+
+This object could be useful if we are often looking up people by their unique ID.
+
+Write a function called `peopleById` that takes an array of people and returns an object where each person
+is keyed by their unique ID.
+
+You have effectively created what we call an *index*, not unlike the one you have in your phonebook.*/
+
+
+function peopleById(){
+    
+}
+
+/*## Find by First Name
+Expanding on the previous exercise, this time we are going to create an index on **first names**.
+Notice how in the previous exercise, each ID was unique. In this case, two people have the same first name.
+
+We want to create a function called `peopleByFirstName` that will take an array of people and
+return something that looks like this:
+
+```json
+{
+  "John": [
+    {
+      "id": "KeXoYg92is",
+      "firstName": "John",
+      "lastName": "Smith",
+      "email": "john@smith.com"
+    },
+    {
+      "id": "m7LPbJYSUg",
+      "firstName": "John",
+      "lastName": "Vader",
+      "email": "vader@darkside.com"
+    }
+  ],
+  "Donald": [
+    {
+      "id": "NkALmSWtUp",
+      "firstName": "Donald",
+      "lastName": "Duck",
+      "email": "don@disney.com"
+    }
+  ]
+}
+```
+
+You have effectively created an *index on first name*. This lets you find all people called, say, 
+"John" without having to look through the whole results.
+
+## High? Low? Part 2!
+Expanding on a previous exercise, write a function called `highLowTwo` that takes an array of numbers, 
+and returns the higest, second highest, lowest, and second lowest numbers.
+
+For example, starting with `[1, -10, 20, 40, 5]`, your function should return:
+
+```json
+{
+  "highest": 40,
+  "secondHighest": 20,
+  "lowest": -10,
+  "secondLowest": 5
+}
 ```
 */
